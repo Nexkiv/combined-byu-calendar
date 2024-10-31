9a986767fc627afc6f0514a04e5409d917299c30
nexkiv <kevin@karatassos.com>
2024-10-31 14:01:16 -0600

    Update(app.jsx) established template

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+    return (
+        <div className='body bg-dark text-light'>
+            <header className='container-fluid'>
+                <h1 className="title">BYU Combined Calender <img src="public/favicon.png" alt="Y calendar" height="50px"></img></h1>
+            </header>
+
+            <main>App components go here</main>
+
+            <footer className="container-fluid">
+                <span className="text-reset">Created By: Kevin Peter Karatassos</span>
+                <a href="https://github.com/Nexkiv/combined-byu-calendar">GitHub Source</a>
+            </footer>
+        </div>
+    );
 }
