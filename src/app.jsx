2ca60a29202bda9a31f8d20d78f2eeb74ba754ab
nexkiv <kevin@karatassos.com>
2024-11-09 16:27:42 -0700

    Update(Calendar) converted function to class

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
-import { Calendar } from './calendar/calendar';
+import Calendar from './calendar/calendar';
 import { AuthState } from './login/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
