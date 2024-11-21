15cbaec57b9d5defda887e743d87157101e5b871
nexkiv <kevin@karatassos.com>
2024-11-21 15:39:41 -0700

    Update (index.js) using simon code

--- service/index.js
+++ service/index.js
@@ -1,6 +1,11 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
+
+const authCookieName = 'token';
 
 // The users are saved in memory and disappear whenever the service is restarted.
 let users = {};
@@ -14,45 +19,84 @@ app.use(express.json());
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
 
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
+
 // Router for service endpoints
-var apiRouter = express.Router();
+const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// CreateAuth a new user
+// CreateAuth token for a new user
 apiRouter.post('/auth/create', async (req, res) => {
-    const user = users[req.body.email];
-    if (user) {
+    if (await DB.getUser(req.body.email)) {
         res.status(409).send({ msg: 'Existing user' });
     } else {
-        const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-        users[user.email] = user;
+        const user = await DB.createUser(req.body.email, req.body.password);
 
-        res.send({ token: user.token });
+        // Set the cookie
+        setAuthCookie(res, user.token);
+
+        res.send({
+            id: user._id,
+        });
     }
 });
 
-// GetAuth login an existing user
+// GetAuth login an existing user using provided credentials
 apiRouter.post('/auth/login', async (req, res) => {
-    const user = users[req.body.email];
+    const user = await DB.getUser(req.body.email);
     if (user) {
-        if (req.body.password === user.password) {
-            user.token = uuid.v4();
-            res.send({ token: user.token });
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            setAuthCookie(res, user.token);
+            res.send({ id: user._id });
             return;
         }
     }
     res.status(401).send({ msg: 'Unauthorized' });
 });
 
-// DeleteAuth logout a user
-apiRouter.delete('/auth/logout', (req, res) => {
-    const user = Object.values(users).find((u) => u.token === req.body.token);
-    if (user) {
-        delete user.token;
-    }
+// DeleteAuth logout a user by deleting the token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+    res.clearCookie(authCookieName);
     res.status(204).end();
 });
 
-app.listen(port, () => {
+// secureApiRouter verifies credentials for endpoints
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+    const authToken = req.cookies[authCookieName];
+    const user = await DB.getUserByToken(authToken);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
+const httpService = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
